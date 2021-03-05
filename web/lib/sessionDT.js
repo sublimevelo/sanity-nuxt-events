fd16006850f6be93c2170277327c50d557b91627
Chris Hughes <chughes@shc.edu>
2021-03-05 15:46:33 -0600

    More flexible date/time formatter

--- web/lib/sessionDT.js
+++ web/lib/sessionDT.js
@@ -2,9 +2,23 @@ import { parseISO } from 'date-fns'
 import { format } from 'date-fns'
 import { add } from 'date-fns'
 
-export default function sessionDT(from, dur) {
-  return `${format(parseISO(from), 'MMMM dd, ha')} - ${format(
-    add(parseISO(from), { minutes: dur }),
-    'ha'
-  )}`
+export default function sessionDT(
+  from,
+  dur,
+  showYear = false,
+  showTime = true
+) {
+  let df = 'MMM dd'
+  let endTime = ''
+
+  if (showYear) {
+    df = `${df}, yyyy`
+  }
+
+  if (showTime) {
+    df = `${df}, ha`
+    endTime = ` - ${format(add(parseISO(from), { minutes: dur }), 'ha')}`
+  }
+
+  return `${format(parseISO(from), df)}${endTime}`
 }
