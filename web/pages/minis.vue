acc6ad331c65e8c87233d0a86a6c1aac5e9efa46
Chris Hughes <chughes@shc.edu>
2021-03-05 15:47:05 -0600

    Clean up code

--- web/pages/minis.vue
+++ web/pages/minis.vue
@@ -1,23 +1,25 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col sm="12">
-        <div>
-          <b-nav pills small>
-            <b-nav-item
-              v-for="item in minisNavItems"
-              :active="$route.path == item.url"
-              :key="item.url"
-              :href="item.url"
-            >
-              {{ item.title }}
-            </b-nav-item>
-          </b-nav>
-        </div>
-        <NuxtChild />
-      </b-col>
-    </b-row>
-  </b-container>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col sm="12">
+          <div>
+            <b-nav pills small>
+              <b-nav-item
+                v-for="item in minisNavItems"
+                :active="$route.path == item.url"
+                :key="item.url"
+                :href="item.url"
+              >
+                {{ item.title }}
+              </b-nav-item>
+            </b-nav>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+    <NuxtChild />
+  </div>
 </template>
 <script>
 import minisNavItems from '~/lib/minisNavItems'
