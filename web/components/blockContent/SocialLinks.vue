3cd81981ea86708b5778ed727c6860b22ce80ac6
Chris Hughes <chughes@shc.edu>
2021-03-05 15:41:07 -0600

    Start list of speakers

---
+++ web/components/blockContent/SocialLinks.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span v-for="social in socials" :key="social._id">
+      <b-link :href="social.url">
+        <b-icon
+          :icon="getIcon(social.title)"
+          class="h2 mr-2 rounded bg-primary p-1"
+          variant="light"
+        ></b-icon
+      ></b-link>
+    </span>
+  </div>
+</template>
+
+<script>
+const socialsMap = {
+  li: { title: 'LinkedIn', icon: 'linkedin' },
+  tw: { title: 'Twitter', icon: 'twitter' },
+  fb: { title: 'Facebook', icon: 'facebook' }
+}
+
+export default {
+  props: {
+    socials: {
+      type: Array
+    }
+  },
+  methods: {
+    socialTitle: key => {
+      return socialsMap[key].title
+    },
+    getIcon: key => {
+      return socialsMap[key].icon
+    }
+  }
+}
+</script>
---
+++ web/pages/minis/speakers/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col sm="12">
+        <div v-for="person in people" :key="person._id">
+          <h2>{{ person.first }} {{ person.last }}</h2>
+          <p>{{ person.title }}, {{ person.institution }}</p>
+          <BlockContent :blocks="person.bio" />
+          <h3>Socials</h3>
+          <SocialLinks :socials="person.socials" />
+          <h3>Sessions</h3>
+          <ul>
+            <li v-for="session in person.sessions" :key="session._id">
+              {{ session.title }}
+            </li>
+          </ul>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import sanityClient from '~/sanityClient'
+import BlockContent from 'sanity-blocks-vue-component'
+import SocialLinks from '~/components/blockContent/SocialLinks'
+// import SeriesSessionList from '~/components/series/SeriesSessionList'
+// import SeriesArchiveList from '~/components/series/SeriesArchiveList'
+// import MoreInfoForm from '~/components/forms/MoreInfoForm'
+
+import { filter, sortBy, partition } from 'lodash'
+import add from 'date-fns/add'
+
+// groq can't deep filter on arrays, so we'll need to filter out dates in script :(
+const query = `
+{
+    'people': *[_type == "person"]{
+    ...,
+    image {
+        alt,
+        "url": asset->url
+        },
+    "sessions": *[
+        _type == "seriesSession"
+        && references(^._id)
+    ]
+    }
+}
+`
+
+export default {
+  components: {
+    SocialLinks,
+    BlockContent
+  },
+  async asyncData() {
+    let data = await sanityClient.fetch(query)
+
+    data.people = filter(data.people, function(o) {
+      return o.sessions.length > 0
+    })
+
+    return data
+  }
+  // head() {
+  //   if (!this.haveInfo) {
+  //     return
+  //   }
+  //   return {
+  //     title: this.info.name,
+  //     meta: [
+  //       {
+  //         hid: 'description',
+  //         name: 'description',
+  //         content: this.info.description
+  //       }
+  //     ]
+  //   }
+  // }
+}
+</script>
+
+<style scoped lang="scss"></style>
