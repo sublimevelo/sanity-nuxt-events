1bfd2662def29d91410a8a2b56154ecfab60879b
Chris Hughes <chughes@shc.edu>
2021-03-06 09:32:47 -0600

    Flesh out common site vars

--- web/plugins/siteVars.js
+++ web/plugins/siteVars.js
@@ -1,8 +1,14 @@
 const rootURL = 'https://leadphilanthropy.com'
+const companyName = 'LEAD Philanthropy'
+const companyURL = 'https://leadphilanthropy.com'
 
 export default ({ app }, inject) => {
   // Inject $hello(msg) in Vue, context and store.
   inject('siteVars', () => {
-    return { rootURL: rootURL }
+    return {
+      rootURL: rootURL,
+      companyName: companyName,
+      companyURL: companyURL
+    }
   })
 }
