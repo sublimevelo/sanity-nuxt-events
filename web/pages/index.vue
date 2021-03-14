0bfef47cae9f8406a9f748a752e432eef6aee1bf
Chris Hughes <chughes@shc.edu>
2021-03-14 13:26:14 -0500

    Better dummy content

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,48 +1,56 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col sm="12">
-        <header class="header">
-          <h1 class="title">Dummy homepage placeholder</h1>
-        </header>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col sm="12">
-        <p>
-          Non voluptate cupidatat incididunt fugiat pariatur velit sint irure id
-          eiusmod proident irure. Consequat ea eu mollit aute Lorem Lorem tempor
-          ipsum voluptate eiusmod ex tempor. Quis laboris ipsum officia do quis
-          quis ullamco culpa duis. Eu labore ea cupidatat et commodo. Non nulla
-          aliquip ullamco pariatur aute fugiat eiusmod adipisicing. Est velit
-          fugiat exercitation id nisi adipisicing ut. Minim reprehenderit velit
-          magna laboris aute occaecat cillum deserunt magna consectetur veniam
-          id aute est.
-        </p>
-
-        <p>
-          Amet occaecat laboris ullamco dolore incididunt occaecat aliqua
-          occaecat veniam non proident ad. Officia esse pariatur aliquip minim
-          pariatur sit quis irure. Veniam nisi reprehenderit est laboris et
-          laboris dolor ut dolor. Sint irure labore quis cupidatat exercitation
-          ea sunt ad et aliqua quis. Consequat ea pariatur nostrud eu ex nulla
-          ad aliqua duis nisi fugiat ut. Reprehenderit ea tempor consectetur sit
-          est elit ut.
-        </p>
-
-        <p>
-          Culpa id labore dolore mollit nostrud aliqua aute excepteur deserunt
-          eiusmod enim. Eiusmod irure enim velit ad qui eu incididunt ullamco ea
-          veniam sit ut cillum voluptate. Sit cupidatat velit non voluptate
-          aliquip commodo. Ad laboris pariatur nisi excepteur amet. Amet
-          exercitation nostrud aute excepteur laboris deserunt id duis minim
-          magna nulla id.
-        </p>
-      </b-col>
-    </b-row>
-  </b-container>
+  <div>
+    <b-container class="mt-3">
+      <!-- <b-row>
+        <b-col sm="12">
+          <header class="header">
+    
+          </header>
+        </b-col>
+      </b-row> -->
+      <b-row>
+        <b-col sm="12">
+          <h1>LEAD Philanthropy</h1>
+          <!-- <p>
+            Jarallax is an open-source javascript library which makes adjusting
+            css based on interaction easy. With Jarallax it's easy to create a
+            parallax scrolling website.
+          </p> -->
+          <p>
+            Non voluptate cupidatat incididunt fugiat pariatur velit sint irure
+            id eiusmod proident irure. Consequat ea eu mollit aute Lorem Lorem
+            tempor ipsum voluptate eiusmod ex tempor. Quis laboris ipsum officia
+            do quis quis ullamco culpa duis. Eu labore ea cupidatat et commodo.
+            Non nulla aliquip ullamco pariatur aute fugiat eiusmod adipisicing.
+            Est velit fugiat exercitation id nisi adipisicing ut. Minim
+            reprehenderit velit magna laboris aute occaecat cillum deserunt
+            magna consectetur veniam id aute est.
+          </p>
+          <p>
+            Amet occaecat laboris ullamco dolore incididunt occaecat aliqua
+            occaecat veniam non proident ad. Officia esse pariatur aliquip minim
+            pariatur sit quis irure. Veniam nisi reprehenderit est laboris et
+            laboris dolor ut dolor. Sint irure labore quis cupidatat
+            exercitation ea sunt ad et aliqua quis. Consequat ea pariatur
+            nostrud eu ex nulla ad aliqua duis nisi fugiat ut. Reprehenderit ea
+            tempor consectetur sit est elit ut.
+          </p>
+          <p>
+            Culpa id labore dolore mollit nostrud aliqua aute excepteur deserunt
+            eiusmod enim. Eiusmod irure enim velit ad qui eu incididunt ullamco
+            ea veniam sit ut cillum voluptate. Sit cupidatat velit non voluptate
+            aliquip commodo. Ad laboris pariatur nisi excepteur amet. Amet
+            exercitation nostrud aute excepteur laboris deserunt id duis minim
+            magna nulla id.
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {}
+</script>
 
 <style scoped></style>
