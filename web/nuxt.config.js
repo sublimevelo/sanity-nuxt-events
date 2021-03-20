17bdae5e627bf5f1eb2b9ee6976541888b093522
Chris Hughes <chughes@shc.edu>
2021-03-20 08:25:24 -0500

    Update for theme

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -1,140 +1,93 @@
-import pkg from './package'
-import sanityClient from './sanityClient'
-
-// const routesQuery = `
-//   {
-//     "conferenceSessions": *[_type == "conferenceSession"],
-//     "speakers": *[_type == "person" && defined(slug.current)]
-//   }
-// `
-
-export default {
-  target: 'static', // default is 'server'
-
+module.exports = {
   /*
    ** Headers of the page
    */
+  router: {
+    base: '/',
+    linkExactActiveClass: 'active'
+  },
   head: {
-    title: pkg.name,
+    title: 'LEAD Philanthropy',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1.0, maximum-scale=1.5, user-scalable=1, shrink-to-fit=no'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Montserrat:400,700,200|Open+Sans+Condensed:700'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.0.6/css/all.css'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css',
+        crossorigin: 'anonymous'
+      }
     ]
-    //link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
   /*
    ** Customize the progress-bar color
    */
   loading: { color: '#ccc' },
-
   /*
    ** Global CSS
    */
-  css: [{ src: '~/styles/app.scss' }],
-  // css: [{ src: '~/styles/app.scss' }, { src: '~/assets/landkit-theme/dist/assets/css/theme.min.css' }],
-  // css: [{ src: '~/assets/landkit-theme/dist/assets/css/theme.min.css' }],
-
+  css: ['~/assets/sass/now-ui-kit.scss', '~/assets/sass/demo.scss'],
   /*
    ** Plugins to load before mounting the App
    */
   plugins: [
-    '~/plugins/debug',
-    '~/plugins/imageBuilder',
-    '~/plugins/siteSettings',
-    '~/plugins/personName',
-    { src: '~/plugins/parallax.js', ssr: false }
+    { src: '~/plugins/globalDirectives.js', ssr: false },
+    { src: '~/plugins/element-ui.js' },
+    { src: '~/plugins/now-ui-kit' },
+    { src: '~/plugins/debug' },
+    { src: '~/plugins/imageBuilder' },
+    { src: '~/plugins/siteSettings' },
+    { src: '~/plugins/personName' }
   ],
-
   /*
    ** Nuxt.js modules
    */
-  // modules: ['@nuxtjs/pwa'],
   modules: ['@nuxtjs/pwa', 'bootstrap-vue/nuxt'],
-  bootstrapVue: {
-    bootstrapCSS: false, // Or `css: false`
-    bootstrapVueCSS: false // Or `bvCSS: false`
-  },
 
   bootstrapVue: {
     // Install the `IconsPlugin` plugin (in addition to `BootstrapVue` plugin)
     icons: true
   },
-
-  /*
-   ** Set global info from sanity document
-   */
-  // conference: () => {
-  //   return sanityClient.fetch('*[_id == "conference"]').then(res => res)
-  // },
-  // conference: () => {
-  //   return sanityClient
-  //     .fetch(
-  //       `
-  //   {
-  //     "info": *[_id == "conference"] | order(conference.schedule.from) {
-  //       ..., image { ..., asset->}
-  //     }[0]
-  //   }
-  // `
-  //     )
-  //     .then(res => res)
-  // },
-
-  /*
-   ** Generate dynamic routes from data from sanity.
-   ** Used only for generating static served HTML files
-   */
-  // generate: {
-  //   routes: () => {
-  //     return sanityClient.fetch(routesQuery).then(res => {
-  //       return [
-  //         ...res.conferenceSessions.map(
-  //           item => `/sessions/${item._id}`
-  //         ),
-  //         ...res.speakers.map(item => `/speakers/${item.slug.current}`)
-  //       ]
-  //     })
-  //   }
-  // },
-
   /*
    ** Build configuration
    */
   build: {
-    postcss: {
-      plugins: {
-        'postcss-import': {},
-        'postcss-preset-env': {
-          stage: 3,
-          features: {
-            'color-mod-function': { unresolved: 'warn' },
-            'nesting-rules': true,
-            'custom-media-queries': {
-              preserve: false
-            },
-            'custom-properties': {
-              preserve: false
-            }
+    extractCSS: process.env.NODE_ENV === 'production',
+    babel: {
+      plugins: [
+        [
+          'component',
+          {
+            libraryName: 'element-ui',
+            styleLibraryName: 'theme-chalk'
           }
-        }
-      }
-    }
-
+        ]
+      ]
+    },
     /*
      ** You can extend webpack config here
      */
-    // extend(config, ctx) {
-    //   // Run ESLint on save
-    //   if (ctx.isDev && ctx.isClient) {
-    //     config.module.rules.push({
-    //       enforce: 'pre',
-    //       test: /\.(js|vue)$/,
-    //       loader: 'eslint-loader',
-    //       exclude: /(node_modules)/
-    //     })
-    //   }
-    // }
+    extend(config, ctx) {}
   }
 }
