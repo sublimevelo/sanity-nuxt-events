71fbcb9f9a3cd56cfbc3ebadc4d0aa90ef50f85a
Chris Hughes <chughes@shc.edu>
2021-03-20 08:19:13 -0500

    Clean up boilerplate code

--- web/plugins/imageBuilder.js
+++ web/plugins/imageBuilder.js
@@ -3,7 +3,6 @@ import imageUrlBuilder from '@sanity/image-url'
 const builder = imageUrlBuilder(sanityClient)
 
 export default ({ app }, inject) => {
-  // Inject $hello(msg) in Vue, context and store.
   inject('urlFor', source => {
     return builder.image(source)
   })
--- web/plugins/personName.js
+++ web/plugins/personName.js
@@ -1,5 +1,4 @@
 export default ({ app }, inject) => {
-  // Inject $hello(msg) in Vue, context and store.
   inject('personName', person => {
     return `${person.first} ${person.last}`
   })
