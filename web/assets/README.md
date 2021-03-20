29f76ab343fe2413b545a804ecb8a77d21a1c9da
Chris Hughes <chughes@shc.edu>
2021-03-19 20:27:07 -0500

    New theme 'Now UI Kit'

--- web/assets/README.md
+++ web/assets/README.md
@@ -1,4 +1,5 @@
-# ASSETS
+fonts
+sass# ASSETS
 
 **This directory is not required, you can delete it if you don't want to use it.**
 
--- web/plugins/parallax.js
+++
@@ -1,9 +0,0 @@
-import 'object-fit-images'
-import { jarallax } from 'jarallax'
-
-window.addEventListener('load', function(event) {
-  let jarallaxers = document.querySelectorAll('.jarallax')
-  for (let j of jarallaxers) {
-    jarallax(j, { speed: j.dataset.speed || 0.8 })
-  }
-})
