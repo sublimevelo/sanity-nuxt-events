cc671939351b402044f59b1adc0cf3b1d4713a9c
Chris Hughes <chughes@shc.edu>
2021-03-20 08:21:00 -0500

    Clean up code

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,33 +1,18 @@
 <template>
   <div>
-    <b-alert v-if="siteSettings.debug" variant="danger" show
-      >Debug mode ON</b-alert
-    >
-    <Navbar :title="siteSettings.siteName" />
+    <main-navbar :color-on-scroll="450"></main-navbar>
     <nuxt />
-    <Footer />
+    <!-- <main-footer></main-footer> -->
   </div>
 </template>
-
 <script>
-import Navbar from '~/components/Navbar.vue'
-import Footer from '~/components/Footer.vue'
-
+import MainNavbar from '@/components/nav/MainNavbar.vue'
+// import MainFooter from '@/components/nav/MainFooter.vue'
 export default {
   components: {
-    Navbar,
-    Footer
-  },
-  data: data => {
-    if (!data.$store.state) {
-      return
-    }
-    return {
-      year: new Date().getFullYear(),
-      siteSettings: data.$store.state.siteSettings
-    }
+    MainNavbar
+    // MainFooter
   }
 }
 </script>
-
 <style></style>
